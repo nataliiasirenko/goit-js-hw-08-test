23db8be43c271bc21b76d35a278667d559023e1c
nataliiasirenko <sirenko.nataliia@hotmail.com>
2022-12-13 15:59:03 +0000

    update

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+      },
+    ],
+  },
+  devServer: {
+    port: 7777,
+  },
+};
